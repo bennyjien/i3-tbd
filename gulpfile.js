0e0e996636590a2f940fd7f0db92cedb9c8dbb4a
Benny Jien <benny@jien.me>
2017-12-18 12:31:18 +0700

    update article and vendor HTML

--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,7 @@ gulp.task('browserSync', function() {
 gulp.task('kit-js-dist', function() {
 	return gulp.src('kit/**/*.kit')
 		.pipe(kit())
-		.pipe(prettify({indent_char: '\t', indent_size: 1, preserve_newlines: true, unformatted: ['a', 'span', 'img', 'code', 'pre', 'sub', 'sup', 'em', 'strong', 'b', 'i', 'u', 'strike', 'big', 'small', 'pre', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'svg','br', 'label', 'input', 'script'], wrap_line_length: 0}))
+		.pipe(prettify({indent_char: '\t', indent_size: 1, preserve_newlines: true, unformatted: ['a', 'span', 'img', 'code', 'pre', 'sub', 'sup', 'em', 'strong', 'b', 'i', 'u', 'strike', 'big', 'small', 'pre', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'svg','br', 'label', 'input', 'script', 'time'], wrap_line_length: 0}))
 		.pipe(useref({ searchPath: './' }))
 		.pipe(gulpIf('bundle.js', babel({ presets: ['es2015'] })))
 		.pipe(gulpIf('bundle.js', uglify()))
